4e9dc1742026f8285b77fefac7754ccd01e3d530
isabell0099 <120369952+isabell0099@users.noreply.github.com>
2024-01-19 17:59:18 +0100

    update_18uhr

--- Izzy_2/src/components/Quiztest.svelte
+++ Izzy_2/src/components/Quiztest.svelte
@@ -1,131 +1,118 @@
 <script>
-    import StartQuiz from "./StartQuiz.svelte";
-    import CorrectAnswer from "./CorrectAnswer.svelte";
-    import IncorrectAnswer from "./IncorrectAnswer.svelte";
-    import { onMount } from "svelte";
-    let dinoJson = [];
-    let quizJson = [];
-    let numbers;
-    let questions = [];
-    let quizStarted = false;
-    let currentQuestionIndex = 0;
-    let QuestionArray = [];
-  
-    // Variablen für randomFrage
-    let AnzahlDinos = 32;
-    let RandomZahlen = [];
-    let DinoArray = [];
-    let json1;
-    let currentKeyToCompare = '';
+  import StartQuiz from "./StartQuiz.svelte";
+  import CorrectAnswer from "./CorrectAnswer.svelte";
+  import IncorrectAnswer from "./IncorrectAnswer.svelte";
+  import { onMount } from "svelte";
+  let dinoJson = [];
+  let quizJson = [];
+  let numbers;
+  let questions = [];
+  let quizStarted = false;
+  let currentQuestionIndex = 0;
+  let QuestionArray = [];
 
-    let showQuizContent = true;
-    let showCorrectAnswer = false;
-    let showIncorrectAnswer = false;
-    
-   
-  
-    function getRandoms(max) {
-      let values = [];
-      for (let i = 0; i < 3; i++) {
-        while (!values[i] && values[i] != 0) {
-          let value = Math.random() * max;
-          value = Math.round(value);
-          if (!values.includes(value)) {
-            values[i] = value;
-          }
+  // Variablen für randomFrage
+  let AnzahlDinos = 32;
+  let RandomZahlen = [];
+  let DinoArray = [];
+  let json1;
+  let currentKeyToCompare = "";
+
+  let showQuizContent = false;
+  let showCorrectAnswer = false;
+  let showIncorrectAnswer = false;
+
+  function getRandoms(max) {
+    let values = [];
+    for (let i = 0; i < 3; i++) {
+      while (!values[i] && values[i] != 0) {
+        let value = Math.random() * max;
+        value = Math.round(value);
+        if (!values.includes(value)) {
+          values[i] = value;
         }
       }
-      return values;
     }
-  
-   
-  
-  
-  
-    // onMount(async () => {
-    // 	const response1 = await fetch("./src/assets/data.json");
-    //     const response2 = await fetch("./src/assets/quiz.json");
-    // 	const json1 = await response1.json();
-    //     const json2 = await response2.json();
-    // 	dinoJson = json1;
-    //     quizJson = json2;
-    //     numbers = getRandoms((quizJson.length)-1);
-    //     numbers.forEach(num => {
-    //         console.log(num);
-    //         questions.push(quizJson[num]);
-    //     });
-    //     console.log(questions);
-    // });
-  
-    async function method() {
-      const response1 = await fetch("./src/assets/data.json");
-      const response2 = await fetch("./src/assets/quizData.json");
-      const json1 = await response1.json();
-      const json2 = await response2.json();
-      dinoJson = json1;
-      quizJson = json2;
-  
-      numbers = getRandoms(quizJson.length - 1);
-      RandomZahlen = []; // Zurücksetzen der RandomZahlen für jede Frage 
-      DinoArray = []; // Zurücksetzen des DinoArray für jede Frage
+    return values;
+  }
+
+  // onMount(async () => {
+  // 	const response1 = await fetch("./src/assets/data.json");
+  //     const response2 = await fetch("./src/assets/quiz.json");
+  // 	const json1 = await response1.json();
+  //     const json2 = await response2.json();
+  // 	dinoJson = json1;
+  //     quizJson = json2;
+  //     numbers = getRandoms((quizJson.length)-1);
+  //     numbers.forEach(num => {
+  //         console.log(num);
+  //         questions.push(quizJson[num]);
+  //     });
+  //     console.log(questions);
+  // });
+
+  async function method() {
+    const response1 = await fetch("./src/assets/data.json");
+    const response2 = await fetch("./src/assets/quizData.json");
+    const json1 = await response1.json();
+    const json2 = await response2.json();
+    dinoJson = json1;
+    quizJson = json2;
+
+    numbers = getRandoms(quizJson.length - 1);
+    RandomZahlen = []; // Zurücksetzen der RandomZahlen für jede Frage
+    DinoArray = []; // Zurücksetzen des DinoArray für jede Frage
 
     //   let randomIndex = Math.floor(Math.random() * numbers.length);
     //   currentKeyToCompare = quizJson[numbers[randomIndex]].key; // Setze den aktuellen key-Wert
     //   console.log(currentKeyToCompare);
-    //   await RandomQuestionManyDinos(); 
+    //   await RandomQuestionManyDinos();
 
     //   numbers.forEach((num) => {
     //     console.log(num);
-        
+
     //     questions.push(quizJson[num]);
     //   });
 
     for (let i = 0; i < numbers.length; i++) {
-    let currentKeyIndex = numbers[i];
-    currentKeyToCompare = quizJson[currentKeyIndex].key; // Setze den aktuellen key-Wert in der Reihenfolge der gestellten Fragen
-    console.log(currentKeyToCompare);
+      let currentKeyIndex = numbers[i];
+      currentKeyToCompare = quizJson[currentKeyIndex].key; // Setze den aktuellen key-Wert in der Reihenfolge der gestellten Fragen
+      console.log(currentKeyToCompare);
 
-    await RandomQuestionManyDinos();
+      await RandomQuestionManyDinos();
 
-    console.log(numbers[i]);
+      console.log(numbers[i]);
 
-    questions.push(quizJson[currentKeyIndex]);
-  }
-      
-
-      return questions;
-
-     
+      questions.push(quizJson[currentKeyIndex]);
     }
-  
-    async function RandomQuestionManyDinos() {
-        DinoArray = [];
-        console.log("Start RandomQuestionManyDinos");
-      const response1 = await fetch("./src/assets/data.json");
-      const json1 = await response1.json();
-      dinoJson = json1;
-  
-     // Vor jeder Frage zurücksetzen
-     RandomZahlen = [];
-    
-  
-      for (let i = 0; i < 4; i++) {
-        let num = Math.floor(Math.random() * AnzahlDinos);
-        while (RandomZahlen.includes(num)) {
-          num = Math.floor(Math.random() * AnzahlDinos);
-        }
-        RandomZahlen.push(num);
-        console.log(num);
-        
-        DinoArray.push(dinoJson[num]);
+
+    return questions;
+  }
+
+  async function RandomQuestionManyDinos() {
+    console.log("Quiz-Teil wird angezeigt")
+    DinoArray = [];
+    console.log("Start RandomQuestionManyDinos");
+    const response1 = await fetch("./src/assets/data.json");
+    const json1 = await response1.json();
+    dinoJson = json1;
+
+    // Vor jeder Frage zurücksetzen
+    RandomZahlen = [];
+
+    for (let i = 0; i < 4; i++) {
+      let num = Math.floor(Math.random() * AnzahlDinos);
+      while (RandomZahlen.includes(num)) {
+        num = Math.floor(Math.random() * AnzahlDinos);
       }
-      console.log(DinoArray);
-      
-      
-      
-      
-    
-      // Find the heaviest dinos    
+      RandomZahlen.push(num);
+      console.log(num);
+
+      DinoArray.push(dinoJson[num]);
+    }
+    console.log(DinoArray);
+
+    // Find the heaviest dinos
     //   let DinoIndex = 0;
     //   console.log(currentKeyToCompare);
     //   for (let i = 1; i < DinoArray.length; i++) {
@@ -139,30 +126,30 @@
     let highestValue = DinoArray[0][currentKeyToCompare];
     let DinoIndex = 0;
 
-  for (let i = 1; i < DinoArray.length; i++) {
-    if (DinoArray[i][currentKeyToCompare] > highestValue) {
-      highestValue = DinoArray[i][currentKeyToCompare];
-      DinoIndex = i;
+    for (let i = 1; i < DinoArray.length; i++) {
+      if (DinoArray[i][currentKeyToCompare] > highestValue) {
+        highestValue = DinoArray[i][currentKeyToCompare];
+        DinoIndex = i;
+      }
     }
+
+    let Solution = DinoArray[DinoIndex];
+    console.log(Solution.name);
+    console.log("End RandomQuestionManyDinos");
+
+    //object für question Array
+    let object = {
+      Key: currentKeyToCompare,
+      Options: DinoArray,
+      Answer: Solution.name,
+    };
+
+    QuestionArray.push(object);
+    console.log(QuestionArray);
   }
 
-  let Solution = DinoArray[DinoIndex];
-   console.log(Solution.name);
-   console.log("End RandomQuestionManyDinos"); 
-
-
-   //object für question Array 
-   let object = {
-    Key : currentKeyToCompare,
-    Options: DinoArray,
-    Answer: Solution.name,
-   }
-
-   QuestionArray.push(object);
-   console.log(QuestionArray);
-    }
-
-    function skipQuestion() { //funktion für skip button
+  function skipQuestion() {
+    //funktion für skip button
     // Prüfe, ob es noch mehr Fragen gibt
     if (currentQuestionIndex < questions.length - 1) {
       currentQuestionIndex++;
@@ -175,7 +162,8 @@
 
   function selectOption(optionIndex) {
     // Hier kannst du die Logik für die ausgewählte Antwortoption implementieren
-    const selectedDino = QuestionArray[currentQuestionIndex].Options[optionIndex];
+    const selectedDino =
+      QuestionArray[currentQuestionIndex].Options[optionIndex];
     const correctAnswer = QuestionArray[currentQuestionIndex].Answer;
 
     if (selectedDino.name === correctAnswer) {
@@ -192,69 +180,69 @@
     // Logik für den Übergang zur nächsten Frage
     if (currentQuestionIndex < QuestionArray.length - 1) {
       currentQuestionIndex++;
-      
+
       RandomQuestionManyDinos(); // Aktualisiere die Dinos für die nächste Frage
       showQuizContent = false; // Setze showQuizContent auf true, um die
       showCorrectAnswer = false;
       showIncorrectAnswer = false;
-
     } else {
       console.log("Ende des Quiz");
       // Hier kannst du weitere Aktionen durchführen, wenn das Quiz zu Ende ist
     }
   }
+  function startQuiz() {
+    // Function to start the quiz
+    quizStarted = true;
+    RandomQuestionManyDinos();
+    showQuizContent = true; // Set showQuizContent to true when starting the quiz
 
-    </script>
-  
-  <main>
-    <div class="quiz-container">
-      {#if !quizStarted}
-        <StartQuiz on:startQuiz={() => (quizStarted = true)} />
-      {/if}
-  
-      {#if quizStarted && showQuizContent }
-       
-        <h1>Quiz</h1>
-        {#await method()}
-          <p>Loading ...</p>
-        {:then value}
-          
+  }
+
+</script>
+
+<main>
+  <div class="quiz-container">
+    {#if !quizStarted}
+      <!-- <StartQuiz on:startQuiz={() => (quizStarted = true)} /> -->
+        <StartQuiz on:startQuiz={startQuiz} />
+    {/if}
+
+    {#if quizStarted && showQuizContent}
+      <h1>Quiz</h1>
+      {#await method()}
+        <p>Loading ...</p>
+      {:then value}
+      
         <!-- {#each value as _, i}
             <h2>{value[i].question}</h2>
           {/each} -->
-    
+        {#if showCorrectAnswer || showIncorrectAnswer}
           {#if value.length > 0 && currentQuestionIndex < value.length}
-          <div>
-            <h2>{value[currentQuestionIndex].question}</h2>
-            {#each QuestionArray[currentQuestionIndex].Options as dino, i}
-            <button on:click={() => selectOption(i)}>{dino.name}</button>
-            {/each}
-          </div>
-          <button on:click={skipQuestion}>Skip</button>
+            <div>
+              <h2>{value[currentQuestionIndex].question}</h2>
+              {#each QuestionArray[currentQuestionIndex].Options as dino, i}
+                <button on:click={() => selectOption(i)}>{dino.name}</button>
+              {/each}
+            </div>
+            <button on:click={skipQuestion}>Skip</button>
+          {/if}
+        {:else}
+          {#if showCorrectAnswer}
+            <CorrectAnswer {onNext} />
+          {/if}
+
+          {#if showIncorrectAnswer}
+            <IncorrectAnswer {onNext} />
+          {/if}
         {/if}
-  
-
-        {#if showCorrectAnswer}
-        <CorrectAnswer onNext={onNext} />
-      {/if}
-
-      {#if showIncorrectAnswer}
-      <IncorrectAnswer onNext={onNext} />
-      {/if}
-
       {/await}
-      
-      {/if}
-      
-    </div>
-  </main>
-  
-  <style>
-    h2 {
-      color: white;
-    }
-    h1 {
-      color: white;
-    }
-   
-  </style>
+    {/if}
+  </div>
+</main>
+
+<style>
+  h1,
+  h2 {
+    color: white;
+  }
+</style>
